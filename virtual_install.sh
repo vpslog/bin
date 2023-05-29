85b7476119ebb9615634b1271071614f909fd681
grayit0 <132592119+grayit0@users.noreply.github.com>
2023-05-29 16:45:02 +0800

    Update virtual_install.sh

--- virtual_install.sh
+++ virtual_install.sh
@@ -5,10 +5,9 @@ set -x
 password="$1"
 
 echo "Detected Debian. Installing OpenSSH..."
-echo -e "${password}\n${password}" | passwd root \
-    && apt-get update \
-    && apt-get install -y openssh-server \
-    && sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config \
-    && sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config \
-    && service ssh restart \
-    && echo "OpenSSH installed successfully."
+/bin/sh -c 'echo -e \"{password}\\n{password}\" | passwd root'\
+            && apt-get update \
+            && apt-get install -y openssh-server \
+            && sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config \
+            && sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config \
+            && service ssh restart \
